9473772ef665b09aa732a94987f92ef3b770316d
zmeyer001 <50673736+zmeyer001@users.noreply.github.com>
2020-03-02 12:33:37 -0600

    Grab relevant weather information for each 3-hour period

--- outreach_method.py
+++ outreach_method.py
@@ -4,6 +4,13 @@ import requests
 # Get data from API
 url = "http://api.openweathermap.org/data/2.5/forecast?q=minneapolis,us&units=imperial&APPID=09110e603c1d5c272f94f64305c09436"
 response = requests.get(url)
-
 data = json.loads(response.text)
-print(json.dumps(data, indent=4))
+
+# Grab the weather information
+for measurement in data["list"]:
+    dt = measurement["dt"]
+    temp = measurement["main"]["temp"]
+    weather = measurement["weather"][0]["main"]
+    sunny = True if weather == "Clear" else False
+    rainy = True if weather == "Rain" else False
+    print(dt, temp, sunny, rainy)
