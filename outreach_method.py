23da51884cba0603bead1df7277811b3c04dbc82
zmeyer001 <50673736+zmeyer001@users.noreply.github.com>
2020-03-02 14:14:31 -0600

    Create function for weather summary

--- outreach_method.py
+++ outreach_method.py
@@ -7,25 +7,41 @@ response = requests.get(url)
 data = json.loads(response.text)
 
 # Grab the weather information
-daily_measures = {}     # keys: date, values: (temp, sunny, rainy)
-for measurement in data["list"]:
-    # If we've already grabbed info about this date, move on
-    date = measurement["dt_txt"].split(" ")[0]
-    if date in daily_measures:
-        continue
+def get_weather_info(data):
+    """
+    Given a response from the OpenWeatherMap.org 5-day/3-hour API, grab a summary of temperature, sunshine, and rain for
+    each day.
+    For now, grab the conditions at noon for each day.  The assumption is that communication will occur in the middle of
+    the day. Later, can create a more elegant summary for the day.
+    Args:
+        data (str): JSON response from the OpenWeatherMap.org API
 
-    # If the time isn't noon, move on
-    time = measurement["dt_txt"].split(" ")[1]
-    if time != "12:00:00":
-        continue
+    Returns:
+        (dict) todo
+    """
+    daily_measures = {}     # keys: date, values: (temp, sunny, rainy)
+    for measurement in data["list"]:
+        # If we've already grabbed info about this date, move on
+        date = measurement["dt_txt"].split(" ")[0]
+        if date in daily_measures:
+            continue
 
-    # Grab the temperature and sunshine/rain conditions
-    temp = measurement["main"]["temp"]
-    weather = measurement["weather"][0]["main"]
-    sunny = True if weather == "Clear" else False
-    rainy = True if weather == "Rain" else False
-    if date not in daily_measures:
-        daily_measures[date] = (temp, sunny, rainy)
+        # If the time isn't noon, move on
+        time = measurement["dt_txt"].split(" ")[1]
+        if time != "12:00:00":
+            continue
 
+        # Grab the temperature and sunshine/rain conditions
+        temp = measurement["main"]["temp"]
+        weather = measurement["weather"][0]["main"]
+        sunny = True if weather == "Clear" else False
+        rainy = True if weather == "Rain" else False
+        if date not in daily_measures:
+            daily_measures[date] = (temp, sunny, rainy)
+
+    return daily_measures
+
+
+daily_measures = get_weather_info(data)
 for day in daily_measures:
     print(day, daily_measures[day])
