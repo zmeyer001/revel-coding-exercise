5234ba8f3f92da68723c14acb5a0dc882cfd69c2
zmeyer001 <50673736+zmeyer001@users.noreply.github.com>
2020-03-02 14:11:03 -0600

    Grab just one measurement per day (at noon)

--- outreach_method.py
+++ outreach_method.py
@@ -7,10 +7,25 @@ response = requests.get(url)
 data = json.loads(response.text)
 
 # Grab the weather information
+daily_measures = {}     # keys: date, values: (temp, sunny, rainy)
 for measurement in data["list"]:
-    dt = measurement["dt"]
+    # If we've already grabbed info about this date, move on
+    date = measurement["dt_txt"].split(" ")[0]
+    if date in daily_measures:
+        continue
+
+    # If the time isn't noon, move on
+    time = measurement["dt_txt"].split(" ")[1]
+    if time != "12:00:00":
+        continue
+
+    # Grab the temperature and sunshine/rain conditions
     temp = measurement["main"]["temp"]
     weather = measurement["weather"][0]["main"]
     sunny = True if weather == "Clear" else False
     rainy = True if weather == "Rain" else False
-    print(dt, temp, sunny, rainy)
+    if date not in daily_measures:
+        daily_measures[date] = (temp, sunny, rainy)
+
+for day in daily_measures:
+    print(day, daily_measures[day])
